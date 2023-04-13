8c4432c0c3ffc04ff4ba22e07949ffbd4426b7bf
Zhdzd <zahidabdullah18@gmail.com>
2023-04-14 00:28:13 +0800

    working on dom

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function getComputerChoice(){
 
 function singleRound(playerSelection, computerSelection){
     //to return declaration of winner
-    playerSelection = playerSelection.toUpperCase();
+    //playerSelection = playerSelection.toUpperCase();
     let results = "";
     if(playerSelection === "ROCK" && computerSelection === "PAPER"){
         console.log("You lose! Paper beats rock")
@@ -38,43 +38,57 @@ function singleRound(playerSelection, computerSelection){
         console.log("YOU WIN!!!!")
         results = "win";
     }
-   // console.log(">>> You choose: " + playerSelection);
-   // console.log(">>> Computer choose: " + computerSelection);
     console.log(results);
     return results;
 }
 //console.log(singleRound("ScisSors", getComputerChoice()));
 
-function game(){
-    //to play a 5 round game
-    //keep score
-    //report the winner or loser
-    playerSelection = "rock"; 
-   // let playerSelection = prompt("Rock, paper or Scissors?");
-   let playerScore = 0;
-   let compScore = 0;
-    for(let i = 1; i <=5; i++){
-        console.log(`Round ${i}` );
-        let result = singleRound(playerSelection,getComputerChoice());
-        if(result === 'draw'){
-            
-        } else if(result === 'win'){
-            playerScore++;
-        } else {
-            compScore++;
-        }
-    }
-    console.log("==============")
-    console.log("FINAL SCORE");
-    console.log("==============")
-    console.log("Player: " +playerScore + " vs Comp: " + compScore)
-    if(playerScore > compScore){
-        console.log("YOU WIN")
-    } else if(playerScore < compScore){
-        console.log("YOU LOSE")
-    } else {
-        console.log("IT'S A TIE. PLAY AGAIN");
-    }
-   
+// function game(){
+//     //to play a 5 round game
+//     //keep score
+//     //report the winner or loser
+//     playerSelection = "rock"; 
+//    // let playerSelection = prompt("Rock, paper or Scissors?");
+//    let playerScore = 0;
+//    let compScore = 0;
+//     // for(let i = 1; i <=5; i++){
+//     //     console.log(`Round ${i}` );
+//     //     let result = singleRound(playerSelection,getComputerChoice());
+//     //     if(result === 'draw'){
+//     //     } else if(result === 'win'){
+//     //         playerScore++;
+//     //     } else {
+//     //         compScore++;
+//     //     }
+//     // }
+//     console.log("==============")
+//     console.log("FINAL SCORE");
+//     console.log("==============")
+//     console.log("Player: " +playerScore + " vs Comp: " + compScore)
+//     if(playerScore > compScore){
+//         console.log("YOU WIN")
+//     } else if(playerScore < compScore){
+//         console.log("YOU LOSE")
+//     } else {
+//         console.log("IT'S A TIE. PLAY AGAIN");
+//     }
+// }
+//game();
+function test(){
+    alert('test1');
 }
-game();
+const playerRock= document.querySelector('#rock');
+const playerSelect = document.querySelectorAll('button');
+
+
+// playerRock.addEventListener('click', function(){
+//     alert('test')
+// });
+playerSelect.forEach((btn)=>{
+    btn.addEventListener('click',()=>{
+        alert(`Player choice: ${btn.id}`);
+    })
+})
+
+
+
